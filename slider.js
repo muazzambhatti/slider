4c4d91a29a385d76028ef337b8fee20c66af4590
electrify <76642256+muazzambhatti@users.noreply.github.com>
2024-03-20 03:33:38 +0500

    Add files via upload

--- slider.js
+++ slider.js
@@ -1,44 +1,91 @@
 let clickRight = 0;
 let clickLeft = 0;
-let viewWidth = screen.width * 2;
-let transformSize = screen.width / 4;
+let viewWidth;
+let transformSize;
+
+if (screen.width <= 420) {
+    viewWidth = screen.width * 4;
+    transformSize = screen.width / 2;
+} else {
+    viewWidth = screen.width * 2;
+    transformSize = screen.width / 4;
+}
 
 console.log(transformSize);
 
 document.getElementById('slider').style.width = viewWidth + 'px';
 
 
-function slideRight() {    
+function slideRight() {
 
-    if (clickRight < 4) {
-        clickRight++;
-        clickLeft++;
+    if (screen.width <= 420) {
 
-        document.getElementById('slider').style.transform = 'translateX(-' + transformSize*clickRight + 'px)';
+        if (clickRight < 6) {
+            clickRight++;
+            clickLeft++;
+
+            document.getElementById('slider').style.transform = 'translateX(-' + transformSize * clickRight + 'px)';
+
+        }
+        console.log(clickLeft + ' in left , ' + clickRight + ' in right');
+
+    } else {
+
+        if (clickRight < 4) {
+            clickRight++;
+            clickLeft++;
+
+            document.getElementById('slider').style.transform = 'translateX(-' + transformSize * clickRight + 'px)';
+
+        }
+        console.log(clickLeft + ' in left , ' + clickRight + ' in right');
+
 
     }
-    console.log(clickLeft + ' in left , '+clickRight+' in right');
+
 
 }
 
-function slideLeft() {    
+function slideLeft() {
 
-    if (clickLeft != 0 ) {
-        clickLeft--;
-    }
+    if (screen.width <= 420) {
 
-    if (clickLeft < 4) {
-
-        document.getElementById('slider').style.transform = 'translateX(-'+transformSize*clickLeft+'px)';
-        console.log(clickLeft + ' in left ');
-
-        if (clickRight =! 0) {
-            clickRight--;
+        if (clickLeft != 0) {
+            clickLeft--;
         }
-        
+
+        if (clickLeft < 6) {
+
+            document.getElementById('slider').style.transform = 'translateX(-' + transformSize * clickLeft + 'px)';
+            console.log(clickLeft + ' in left ');
+
+            if (clickRight = !0) {
+                clickRight--;
+            }
+
+        }
+
+        console.log(clickLeft + ' in left , ' + clickRight + ' in right');
+
+    } else {
+
+        if (clickLeft != 0) {
+            clickLeft--;
+        }
+
+        if (clickLeft < 4) {
+
+            document.getElementById('slider').style.transform = 'translateX(-' + transformSize * clickLeft + 'px)';
+            console.log(clickLeft + ' in left ');
+
+            if (clickRight = !0) {
+                clickRight--;
+            }
+
+        }
+
+        console.log(clickLeft + ' in left , ' + clickRight + ' in right');
+
     }
-
-    console.log(clickLeft + ' in left , '+clickRight+' in right');
-
 
 }
